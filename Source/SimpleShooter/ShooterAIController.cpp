e3b7d1386a4527b2c2ce579b1ba1449576eebd01
A0L0XIIV <brnky_95+gitkraken@hotmail.com>
2021-02-06 17:07:49 -0500

    195, 196: NavMesh and AI focus-movement

--- Source/SimpleShooter/ShooterAIController.cpp
+++ Source/SimpleShooter/ShooterAIController.cpp
@@ -6,8 +6,23 @@
 
 void AShooterAIController::BeginPlay() {
 	Super::BeginPlay();
+}
+
+void AShooterAIController::Tick(float DeltaSeconds) {
+	Super::Tick(DeltaSeconds);
 	// Get player
 	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
-	// Set AI focus to the player
-	SetFocus(PlayerPawn);
+	
+	if (LineOfSightTo(PlayerPawn)) {
+		// Move towards the player
+		MoveToActor(PlayerPawn, AcceptanceRadius);
+		// Set AI focus to the player
+		SetFocus(PlayerPawn);
+	}
+	else {
+		// Stop movement
+		StopMovement();
+		// Clear AI's gameplay focus
+		ClearFocus(EAIFocusPriority::Gameplay);
+	}
 }
