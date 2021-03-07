b5fb58e88d6329d7c48b305dbe5976e1d7245c5e
A0L0XIIV <brnky_95+gitkraken@hotmail.com>
2021-03-07 16:54:08 -0500

    209, 210: PullTrigger refactored, gun sounds added

--- Source/SimpleShooter/Gun.cpp
+++ Source/SimpleShooter/Gun.cpp
@@ -35,42 +35,61 @@ void AGun::Tick(float DeltaTime)
 
 }
 
-void AGun::PullTrigger()
+
+AController* AGun::GetOwnerController() const
 {
-	// Spawn muzzle flash particle effect at the gun, it is attached so particles moves with gun
-	UGameplayStatics::SpawnEmitterAttached(MuzzleFlash, Mesh, TEXT("MuzzleFlashSocket"));
-
 	APawn* OwnerPawn = Cast<APawn>(GetOwner());
-	if (OwnerPawn == nullptr) return;
-	AController* OwnerController = OwnerPawn->GetController();
-	if (OwnerController == nullptr) return;
+	if (OwnerPawn == nullptr) return nullptr;
+	return OwnerPawn->GetController();
+}
 
+
+bool AGun::GunTrace(FHitResult& Hit, FVector& ShotDirection)
+{
 	FVector Location;
 	FRotator Rotation;
+
+	AController* OwnerController = GetOwnerController();
+	if (OwnerController == nullptr) return false;
 	OwnerController->GetPlayerViewPoint(Location, Rotation);
+
+	// Get Shot direction which is inverse of the rotation
+	ShotDirection = -Rotation.Vector();
 
 	FVector End = Location + Rotation.Vector() * MaxRange;
 
-	FHitResult Hit;
 	FCollisionQueryParams Params;
 	// Ignore gun while shooting
 	Params.AddIgnoredActor(this);
 	// Ignore gun's owner (actor) while shooting
 	Params.AddIgnoredActor(GetOwner());
 	// ECC_GameTraceChannel1 is our custom trace which is Bullet
-	bool hitSuccess = GetWorld()->LineTraceSingleByChannel(Hit, Location, End, ECollisionChannel::ECC_GameTraceChannel1, Params);
+	return GetWorld()->LineTraceSingleByChannel(Hit, Location, End, ECollisionChannel::ECC_GameTraceChannel1, Params);
+}
 
-	if (hitSuccess) {
-		// Get Shot direction which is inverse of the rotation
-		FVector ShotDirection = -Rotation.Vector();
-		// Spawn particals at the hit location
+
+void AGun::PullTrigger()
+{
+	// Spawn muzzle flash particle effect and sound at the gun, it is attached so particles moves with gun
+	UGameplayStatics::SpawnEmitterAttached(MuzzleFlash, Mesh, TEXT("MuzzleFlashSocket"));
+	UGameplayStatics::SpawnSoundAttached(MuzzleSound, Mesh, TEXT("MuzzleFlashSocket"));
+
+	FHitResult Hit;
+	FVector ShotDirection;
+	bool bSuccess = GunTrace(Hit, ShotDirection);
+
+	if (bSuccess) {
+		// Spawn particals and sound at the hit location
 		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ImpactEffect, Hit.Location, ShotDirection.Rotation());
+		UGameplayStatics::PlaySoundAtLocation(GetWorld(), ImpactSound, Hit.Location);
 		// Hit actor 
 		AActor* HitActor = Hit.GetActor();
 		// If hits to actor
 		if (HitActor != nullptr) {
 			// Custom Point Damage Event for Acotr's TakeDamage function
 			FPointDamageEvent DamageEvent(Damage, Hit, ShotDirection, nullptr);
+			// Get the owner controlelr to damage
+			AController* OwnerController = GetOwnerController();
 			// Damage to HitActor
 			Hit.Actor->TakeDamage(Damage, DamageEvent, OwnerController, this);
 		}
